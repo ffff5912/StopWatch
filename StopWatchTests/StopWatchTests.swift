e12088d250b3023ac8d3580ba6e6b5a6069bf469
ffff5912 <iioka5912@gmail.com>
2015-09-26 11:48:00 +0900

    add test

--- StopWatchTests/StopWatchTests.swift
+++ StopWatchTests/StopWatchTests.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 import XCTest
+import RealmSwift
 
 class StopWatchTests: XCTestCase {
     var stopWatch: StopWatch!
+    var recordRepository: RecordRepository!
     var standbyButton = UIButton()
     var recordButton = UIButton()
     
     override func setUp() {
         super.setUp()
         stopWatch = StopWatch(label: UILabel())
+        stopWatch.recordRepository.realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "MyInMemoryRealm"))
     }
     
     override func tearDown() {
@@ -29,13 +32,43 @@ class StopWatchTests: XCTestCase {
         XCTAssert(true, "Pass")
     }
     
-    func testActivation() {
+    func testActiveStopWatch() {
         stopWatch.active(standbyButton, recordButton: recordButton)
         XCTAssertTrue(stopWatch.time != "00:00.00", "タイマーが作動しているか")
         XCTAssertTrue(standbyButton.currentTitle == "STOP", "タイマー動作中のタイトル")
         XCTAssertTrue(recordButton.currentTitle == "LAP", "タイマー動作中のタイトル")
         XCTAssertFalse(stopWatch.standby, "タイマー操作管理フラグ")
         XCTAssertTrue(stopWatch.addLap, "TableViewへの操作管理フラグ")
+    }
+    
+    func testPassiveStopWatch() {
+        stopWatch.passive(standbyButton, recordButton: recordButton)
+        XCTAssertEqual(stopWatch.timer.timer, nil, "タイマーが破棄されているか")
+        XCTAssertTrue(standbyButton.currentTitle == "START", "タイマー動作中のタイトル")
+        XCTAssertTrue(recordButton.currentTitle == "RESET", "タイマー動作中のタイトル")
+        XCTAssertTrue(stopWatch.standby, "タイマー操作管理フラグ")
+        XCTAssertFalse(stopWatch.addLap, "TableViewへの操作管理フラグ")
+    }
+    
+    func testResetStopWatch() {
+        stopWatch.records.add(Record())
+        stopWatch.reset()
+        XCTAssertFalse(stopWatch.addLap, "TableViewへの操作管理フラグ")
+        XCTAssertTrue(stopWatch.records.count == 0)
+        XCTAssertTrue(stopWatch.timer.fractions == 0)
+        XCTAssertTrue(stopWatch.timer.seconds == 0)
+        XCTAssertTrue(stopWatch.timer.minutes == 0)
+        XCTAssertTrue(stopWatch.timer.currentTime == "00:00.00")
+        XCTAssertTrue(stopWatch.timer.stopwatchLabel.text == "00:00.00")
+    }
+    
+    func testUpdate() {
+        stopWatch.timer.currentTime = "11:11.11"
+        stopWatch.update()
+        let record = stopWatch.recordRepository.realm.objects(Record)
+        XCTAssertTrue(record.count == 1, "更新できているか")
+        XCTAssertTrue(stopWatch.records.count == 1, "コレクションに追加できているか")
+        
     }
     
     func testPerformanceExample() {
